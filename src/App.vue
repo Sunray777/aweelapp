1eb5f4e2391ae8f22111dfd1d1bf885386dae9ce
Alexander <you@example.com>
2021-02-27 12:48:57 +0300

    Added a layout and a header

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,29 @@
 <template>
+<b-container>
+  <b-row align-v="center">
+    <b-col>
   <div id="app">
     <div class="container">
+    <div class="header_img">
+  <b-card
+    overlay
+    img-src="https://picsum.photos/900/250/?image=3"
+    img-alt="Card Image"
+    text-variant="white"
+    title="Image Overlay"
+    sub-title="Subtitle"
+  >
+    <b-card-text>
+      Some quick example text to build on the card and make up the bulk of the card's content.
+    </b-card-text>
+  </b-card>
+</div>
 <CarTable class="cartableapp"/>
 </div>
   </div>
+  </b-col>
+  </b-row>
+</b-container>
 </template>
 
 <script>
