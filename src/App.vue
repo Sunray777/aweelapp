53e35cf8345c5701d0e59a484c18b322fff8e8fa
Alexander <you@example.com>
2021-02-27 12:29:02 +0300

    First tries of the A WEEL APP

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,18 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <div class="container">
+<CarTable class="cartableapp"/>
+</div>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import CarTable from './components/CarTable.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    CarTable
   }
 }
 </script>
@@ -25,4 +26,17 @@ export default {
   color: #2c3e50;
   margin-top: 60px;
 }
+.cartableapp 
+{
+  max-width: 1000px;
+  margin: auto;
+}
+.container
+{
+  max-width: 1200px;
+  min-height: 1200px;
+  margin: auto;
+  background: #E3E3E3;
+}
+
 </style>
---
+++ src/components/CarTable.vue
@@ -0,0 +1,28 @@
+<template>
+  <div>
+    <b-table striped hover :items="items"></b-table>
+  </div>
+</template>
+
+<script>
+  export default {
+    data() {
+      return {
+        fields: ['Марка авто', 'Модель', 'Год выпуска'],
+        items: [
+          { Марка_авто: 'Dickerson', Модель: 'Macdonald', Год_выпуска: 1940 },
+          { Марка_авто: 'Dickerson', Модель: 'Macdonald', Год_выпуска: 2018 },
+          { Марка_авто: 'Dickerson', Модель: 'Macdonald', Год_выпуска: 1925 },
+          { Марка_авто: 'Dickerson', Модель: 'Macdonald', Год_выпуска: 1995 },
+          { Марка_авто: 'Dickerson', Модель: 'Macdonald', Год_выпуска: 2020 },
+          
+        ],
+  
+      }
+    }
+  }
+</script>
+
+<style scoped>
+
+</style>
